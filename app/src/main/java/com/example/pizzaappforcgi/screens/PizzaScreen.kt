168146b1f3b957506c5901e9b2663f17411851cc
safak.celik <safak44@live.de>
2023-07-13 19:33:34 +0200

    Deleted unnecessary initialisation

--- app/src/main/java/com/example/pizzaappforcgi/screens/PizzaScreen.kt
+++ app/src/main/java/com/example/pizzaappforcgi/screens/PizzaScreen.kt
@@ -4,6 +4,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import com.example.pizzaappforcgi.components.PizzaRow
 import com.example.pizzaappforcgi.model.Pizza
@@ -11,7 +16,7 @@ import com.example.pizzaappforcgi.ui.theme.CgiDimens
 
 @Composable
 fun PizzaScreen(
-    pizzas: List<Pizza> = emptyList(),
+    pizzas: List<Pizza>,
     onRemoveClicked: (Pizza) -> Unit = {}
 ) {
     LazyColumn(Modifier.padding(CgiDimens.spacings.spacingXS)) {
