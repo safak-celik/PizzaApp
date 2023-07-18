88541b62cde1eb2ba7f25d3db25c7de609f1f0c6
safak.celik <safak44@live.de>
2023-07-18 14:08:02 +0200

    Code Refactoring

--- app/src/main/java/com/example/pizzaappforcgi/base/UiEffect.kt
+++
@@ -1,3 +0,0 @@
-package com.example.pizzaappforcgi.base
-
-interface UiEffect
--- app/src/main/java/com/example/pizzaappforcgi/screens/pizza/PizzaDetailsScreen.kt
+++ app/src/main/java/com/example/pizzaappforcgi/screens/pizza/PizzaDetailsScreen.kt
@@ -8,8 +8,8 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Alignment.Companion.Start
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -27,7 +27,7 @@ fun PizzaDetailsScreen(id: Int, viewModel: ViewModel) {
         Modifier
             .fillMaxSize()
             .padding(CgiDimens.spacings.spacingS),
-        horizontalAlignment = Alignment.Start
+        horizontalAlignment = Start
     ) {
         Image(
             modifier = Modifier
